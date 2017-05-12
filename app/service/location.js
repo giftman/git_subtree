85d123cd6a3b4449abc769b49b0ad33de3f656ca
chufan <121551591@qq.com>
2017-05-12 16:51:51 +0800

    location

--- app/service/location.js
+++ app/service/location.js
@@ -26,10 +26,10 @@ module.exports = app => {
         const errorMsg = result.data && result.data.error_msg ? result.data.error_msg : 'unknown error';
         this.ctx.throw(result.status, errorMsg);
       }
-      if (!result.data.success) {
-        // 远程调用返回格式错误
-        this.ctx.throw(500, 'remote response error', { data: result.data });
-      }
+      // if (!result.data.success) {
+      //   // 远程调用返回格式错误
+      //   this.ctx.throw(500, 'remote response error', { data: result.data });
+      // }
     }
   }
   return LocationService;
